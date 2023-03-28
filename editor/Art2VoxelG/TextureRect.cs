9f8f24de99c4e857f9bb13f8966b7ae683bbc188
turican0 <41465132+turican0@users.noreply.github.com>
2023-03-28 18:53:53 +0200

    230328-update

--- editor/Art2VoxelG/TextureRect.cs
+++ editor/Art2VoxelG/TextureRect.cs
@@ -1,12 +1,53 @@
 using Godot;
 using System;
 
+[Tool]
 public partial class TextureRect : Godot.TextureRect
 {
+    public bool first_run=true;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+        first_run = false;
+        StartRun();
+    }
+
+    private void StartRun()
+    {
         LoadImageAsTexture("res://TMAPS2-0-000-00.pngGr.png");
+        //SetGrid();
+    }
+    //TextureRect childnode;
+    private void SetGrid()
+    {
+        // Získání velikosti TextureRect
+        Vector2 rectSize = this.Texture.GetSize();
+
+        // Vytvoøení nového Image s bílým pozadím, odpovídající velikosti TextureRect
+        //Image gridImage = Image.Create((int)rectSize.X, (int)rectSize.Y, false, Image.Format.Rgba8);
+        //childnode.Texture = new Texture();
+        // Nastavení barvy èar na šedou
+        Color lineColor = new Color(0.5f, 0.5f, 0.5f);
+
+        // Nakreslení vodorovných èar
+        for (int y = 0; y < rectSize.Y; y += 1)
+        {
+            DrawLine(new Vector2(0, y), new Vector2(rectSize.X, y), lineColor);
+        }
+
+        // Nakreslení svislých èar
+        for (int x = 0; x < rectSize.X; x += 1)
+        {
+            DrawLine(new Vector2(x, 0), new Vector2(x, rectSize.Y), lineColor);
+        }
+
+        // Vytvoøení nového Texture z Image
+        //Texture gridTexture = new Texture();
+        //gridTexture.CreateFromImage(gridImage);
+
+        // Nastavení nového Texture na TextureRect
+        //textureRect.Texture = gridTexture;
+        //this.AddChild(childnode);
     }
 
     private void LoadImageAsTexture(string imagePath)
@@ -20,5 +61,46 @@ public partial class TextureRect : Godot.TextureRect
     // Called every frame. 'delta' is the elapsed time since the previous frame.
     public override void _Process(double delta)
 	{
+        if (first_run)
+        {
+            first_run = false;
+            //StartRun();
+        }
 	}
+
+    public override void _Input(InputEvent inputEvent)
+    {
+        if (inputEvent is InputEventMouseButton mouseButtonEvent)
+        {
+            if (mouseButtonEvent.Pressed && mouseButtonEvent.ButtonIndex == MouseButton.Left)
+            {
+                GD.Print("Left mouse button pressed");
+            }
+            else if (mouseButtonEvent.Pressed && mouseButtonEvent.ButtonIndex == MouseButton.Right)
+            {
+                GD.Print("Right mouse button pressed");
+            }
+            else if (mouseButtonEvent.Pressed && mouseButtonEvent.ButtonIndex == MouseButton.WheelUp)
+            {
+                Vector2 currentScale = this.Scale;
+                currentScale.X *= 1.1f;
+                currentScale.Y *= 1.1f;
+                this.Scale = currentScale;
+            }
+            else if (mouseButtonEvent.Pressed && mouseButtonEvent.ButtonIndex == MouseButton.WheelDown)
+            {
+                Vector2 currentScale = this.Scale;
+                currentScale.X *= 0.9f;
+                currentScale.Y *= 0.9f;
+                this.Scale = currentScale;
+            }
+        }
+        else if (inputEvent is InputEventMouseMotion mouseMotionEvent)
+        {
+            if (mouseMotionEvent.Relative != Vector2.Zero)
+            {
+                GD.Print("Mouse moved");
+            }
+        }
+    }
 }
